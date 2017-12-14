b0543f663809b62964375a5c00054b6ff304f341
Mohit3003 <33267674+Mohit3003@users.noreply.github.com>
2017-12-14 20:02:48 +0000

    Add files via upload

--- price-finder/README.md
+++ price-finder/README.md
@@ -1,14 +1,113 @@
-# App name
+# Amazon Price Finder API for Zendesk
 
-[brief description of the app]
+Amazon Price Finder API uses Amazon's Item search operation to look for items which are similair to keyword which is taken from zendesk ticket TV Model field. Retrieves the list of products which when clicked on displays further information about the product.
 
-### The following information is displayed:
+## Amazon Price Finder Sample Input/Output
 
-* info1
-* info2
-* info3
+A GET request is being sent to the API where Authetication is achieved via access key id, associate id and signature and item being searched is sent as Keywords.
 
-Please submit bug reports to [Insert Link](). Pull requests are welcome.
+	### Request Payload
+	
+		http://webservices.amazon.co.uk/onca/xml?
+		Service=AWSECommerceService&
+		AWSAccessKeyId=[AWS Access Key ID]&
+		AssociateTag=[Associate ID]&  
+		Operation=ItemSearch&
+		Keywords=[TV Model Number from the ticket]&
+		SearchIndex=Electronics
+		&Timestamp=[YYYY-MM-DDThh:mm:ssZ]
+		&Signature=[Request Signature]
 
-### Screenshot(s):
-[put your screenshots down here.]
+
+	### Response Payload
+
+	<Item>
+         <ASIN>B073VQB5X3</ASIN>
+         <ParentASIN>B0744K69WQ</ParentASIN>
+         <DetailPageURL>https://www.amazon.co.uk/Toshiba-65U6763DB-65-Inch-Ultra-Freeview/dp/B073VQB5X3?psc=1&amp;SubscriptionId=AKIAJPOPMTPE6XJQEZTQ&amp;tag=legal12345-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=165953&amp;creativeASIN=B073VQB5X3</DetailPageURL>
+         <MediumImage>
+            <URL>https://images-eu.ssl-images-amazon.com/images/I/41hYDQKupGL._SL160_.jpg</URL>
+            <Height Units="pixels">107</Height>
+            <Width Units="pixels">160</Width>
+         </MediumImage>
+         <ItemAttributes>
+            <Binding>Television</Binding>
+            <Brand>Toshiba</Brand>
+            <Color>Black</Color>
+            <EAN>5055862313831</EAN>
+            <EANList>
+               <EANListElement>5055862313831</EANListElement>
+            </EANList>
+            <Feature>Ultra HD (4K)</Feature>
+            <Feature>4x HDMI, 3x USB, 1x Scart</Feature>
+            <Feature>ME/MC (Motion Estimation Motion Compensation)</Feature>
+            <Feature>Toshiba Smart Portal</Feature>
+            <Feature>Internal WLAN &amp; Bluetooth</Feature>
+            <Label>Toshiba</Label>
+            <ListPrice>
+               <Amount>119999</Amount>
+               <CurrencyCode>GBP</CurrencyCode>
+               <FormattedPrice>£1,199.99</FormattedPrice>
+            </ListPrice>
+            <Manufacturer>Toshiba</Manufacturer>
+            <Model>65U6763DB</Model>
+            <MPN>65U6763DB</MPN>
+            <NumberOfItems>1</NumberOfItems>
+            <PackageDimensions>
+               <Height Units="hundredths-inches">787</Height>
+               <Length Units="hundredths-inches">6378</Length>
+               <Weight Units="hundredths-pounds">6614</Weight>
+               <Width Units="hundredths-inches">3937</Width>
+            </PackageDimensions>
+            <PackageQuantity>1</PackageQuantity>
+            <PartNumber>65U6763DB</PartNumber>
+            <ProductGroup>Home Theater</ProductGroup>
+            <ProductTypeName>TELEVISION</ProductTypeName>
+            <Publisher>Toshiba</Publisher>
+            <Size>65-Inch</Size>
+            <Studio>Toshiba</Studio>
+            <Title>Toshiba 65U6763DB 65-Inch Ultra HD LED Smart TV with Freeview Play - Black (2017 Model)</Title>
+         </ItemAttributes>
+         <OfferSummary>
+            <LowestNewPrice>
+               <Amount>79900</Amount>
+               <CurrencyCode>GBP</CurrencyCode>
+               <FormattedPrice>£799.00</FormattedPrice>
+            </LowestNewPrice>
+            <TotalNew>2</TotalNew>
+            <TotalUsed>0</TotalUsed>
+            <TotalCollectible>0</TotalCollectible>
+            <TotalRefurbished>0</TotalRefurbished>
+         </OfferSummary>
+         <Offers>
+            <TotalOffers>1</TotalOffers>
+            <TotalOfferPages>1</TotalOfferPages>
+            <MoreOffersUrl>https://www.amazon.co.uk/gp/offer-listing/B073VQB5X3?SubscriptionId=AKIAJPOPMTPE6XJQEZTQ&amp;tag=legal12345-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=12734&amp;creativeASIN=B073VQB5X3</MoreOffersUrl>
+            <Offer>
+               <OfferAttributes>
+                  <Condition>New</Condition>
+               </OfferAttributes>
+               <OfferListing>
+                  <Price>
+                     <Amount>79900</Amount>
+                     <CurrencyCode>GBP</CurrencyCode>
+                     <FormattedPrice>£799.00</FormattedPrice>
+                  </Price>
+                  <AmountSaved>
+                     <Amount>40099</Amount>
+                     <CurrencyCode>GBP</CurrencyCode>
+                     <FormattedPrice>£400.99</FormattedPrice>
+                  </AmountSaved>
+                  <PercentageSaved>33</PercentageSaved>
+                  <Availability>Usually dispatched within 24 hours</Availability>
+                  <AvailabilityAttributes>
+                     <AvailabilityType>now</AvailabilityType>
+                     <MinimumHours>0</MinimumHours>
+                     <MaximumHours>0</MaximumHours>
+                  </AvailabilityAttributes>
+                  <IsEligibleForSuperSaverShipping>1</IsEligibleForSuperSaverShipping>
+                  <IsEligibleForPrime>0</IsEligibleForPrime>
+               </OfferListing>
+            </Offer>
+         </Offers>
+      </Item>
--- price-finder/assets/main2.js
+++ price-finder/assets/main2.js
@@ -1,27 +1,46 @@
 $(function() {
+  // Initialize ZAF Client	
   var client = ZAFClient.init();
-  client.invoke('resize', { width: '100%', height: '140px' });
+  client.invoke('resize', { width: '100%', height: '50px' });
  
-  var tv_asin,requestData,xml,tv_price,tv_manu,tv_model;
- 
+  var tv_asin,requestData,xml,tv_manu,tv_model;  
+ //	Get values from Zendesk ticket fields
   client.get('ticket.customField:custom_field_114099896612').then(
 	function(data) {
 		tv_asin = data['ticket.customField:custom_field_114099896612'];
-		console.log(tv_asin);//B01GA3Z1M2
-        showInfo(tv_asin,client);
+		console.log(tv_asin);
+		if(tv_asin!="" && tv_asin!=null){
+			showInfo(tv_asin,client);
+		}else{
+			sendData("");
+		}
 	}
   );
+  // Opens list of products in a modal window
+  $(document).on('click',".prodList" ,function(){		
+	client.invoke('instances.create', {
+		location: 'modal',
+		url: 'assets/price-finder-modal.html'
+	});
+  });
+  
+  client.on('modal.close', function() {
+	  console.log("modal closed");
+	  var myArray = JSON.parse(sessionStorage.getItem('myArray'));
+	  sendMyData(myArray);
+  });
+  
 });
-
+//	Method which calls the item search API
 function showInfo(tv_asin,client) {
 	
-	var tv_price;	
-	var access_key_id = "AWSAccessKeyId=YOURAWSACCESSKEYID";
-	var secret_key = "YOURSECRETKEY";
-	var associate_Id= "&AssociateTag=YOURASSOCIATETAG";
+	var tv_asin_encoded = encodeURIComponent(tv_asin.trim());
+	var access_key_id = "AWSAccessKeyId=AKIAJPOPMTPE6XJQEZTQ";
+	var secret_key = "gH9VYlTSXUz8feyuOMCxJa5jQ4mD3/sS2BYTEurY";
+	var associate_Id= "&AssociateTag=legal12345-21";
 	var idType= "&IdType=ASIN";
-	var itemId= "&ItemId="+tv_asin;
-	var operation = "&Operation=ItemLookup";
+	var itemId= "&ItemId="+tv_asin_encoded;
+	var operation = "&Operation=ItemSearch";
 	var responseGroup= "&ResponseGroup=Images%2CItemAttributes%2COffers%2CReviews";
 	var service = "&Service=AWSECommerceService";
 	var UTCstring = new Date().toISOString();
@@ -30,54 +49,97 @@ function showInfo(tv_asin,client) {
 	var timestamp = "&Timestamp=" + encodedUTCstring2;
 	var url = "http://webservices.amazon.co.uk/onca/xml?";
 	var version = "&Version=2017-08-01";
+	var searchIndex= "&SearchIndex=Electronics";
+	var keywords= "&Keywords=" + tv_asin_encoded;
 
-	var sign_string = "GET\nwebservices.amazon.co.uk\n/onca/xml\n"+access_key_id+associate_Id+idType+itemId+operation+responseGroup+service+timestamp+version;
+	var sign_string = "GET\nwebservices.amazon.co.uk\n/onca/xml\n"+access_key_id+associate_Id+keywords+operation+responseGroup+searchIndex+service+timestamp+version;
+	
 	
 	var signature2 = CryptoJS.HmacSHA256(sign_string, secret_key);
 	var abcd= signature2.toString(CryptoJS.enc.Base64);
 	var one  = abcd.replace("+","%2B");
 	var two = one.replace("=", "%3D");
 	var signature = "&Signature=" + two;
-	var endpoint = url+access_key_id+associate_Id+idType+itemId+operation+responseGroup+service+timestamp+version+signature;
+	var endpoint = url+access_key_id+associate_Id+keywords+operation+responseGroup+searchIndex+service+timestamp+version+signature;
+	console.log(endpoint);
 	var settings = {
 		url: endpoint,
 		type: 'GET',
 		dataType: 'json',
 		contentType: 'application/json'
-	};	
+	};
 	
 	client.request(settings).then(
 		function(response) {
 			console.log("response = " + response);
 		},
 		function(data){
-			var parser, xmlDoc;
+			var parser, xmlDoc, itemImage, itemPageUrl, itemMoreOffersUrl, itemTitle, itemPrice;
+			var arrOfItems = [];
+			var myArray=[];
 			parser = new DOMParser();
 			xmlDoc = parser.parseFromString(data.responseText,"text/xml");
 			if(xmlDoc!=undefined){
-				tv_price = xmlDoc.getElementsByTagName("FormattedPrice")[1]!=undefined ? 
-					xmlDoc.getElementsByTagName("FormattedPrice")[1].innerHTML : "Error Occured" ;//OfferSummary.FormattedPrice
-				tv_manu = xmlDoc.getElementsByTagName("Manufacturer")[0]!=undefined ? 
-					xmlDoc.getElementsByTagName("Manufacturer")[0].innerHTML : "Error Occured";
-				tv_model = xmlDoc.getElementsByTagName("Model")[0]!=undefined ? 
-					xmlDoc.getElementsByTagName("Model")[0].innerHTML : "Error Occured";
+				
+				arrOfItems = xmlDoc.getElementsByTagName("Item");
+				console.log(arrOfItems);
+				
+				for(var i=0;i<arrOfItems.length;i++){
+					var moreOffersExist = false;
+					itemImage = xmlDoc.getElementsByTagName("MediumImage")[i].getElementsByTagName("URL")[0].innerHTML;
+					itemPageUrl = xmlDoc.getElementsByTagName("DetailPageURL")[i].innerHTML;
+					itemMoreOffersUrl = xmlDoc.getElementsByTagName("MoreOffersUrl")[i].innerHTML;
+					itemPrice = xmlDoc.getElementsByTagName("LowestNewPrice")[i].getElementsByTagName("FormattedPrice")[0].innerHTML;
+					if(itemMoreOffersUrl.length >1){
+						moreOffersExist = true;
+					}else{
+						moreOffersExist = false;
+					}
+					itemTitle = xmlDoc.getElementsByTagName("ItemAttributes")[i].getElementsByTagName("Title")[0].innerHTML;
+					var data = {
+						'itemImage' : itemImage,
+						'itemPageUrl' :itemPageUrl,
+						'itemMoreOffersUrl': itemMoreOffersUrl,
+						'itemTitle' : itemTitle,
+						'moreOffersExist':moreOffersExist,
+						'itemPrice': itemPrice
+					};
+					myArray.push(data);
+				}
 			}
-			sendData(tv_price,tv_asin, tv_model, tv_manu);			
+			sessionStorage.setItem('myArray',JSON.stringify(myArray));
+			sendData(myArray);
 		}
 	)
 }
  
-function sendData(tv_price, tv_asin, tv_model, tv_manu){
-	var tv_link  = "https://www.amazon.co.uk/dp/" + tv_asin;	
-	 var requester_data = {
-		'tv_price': tv_price,
-		'tv_asin' : tv_asin,
-		'tv_manu' : tv_manu,
-		'tv_model' : tv_model,
-		'tv_link' : tv_link		
+function sendData(myArray){
+	console.log("in send data");
+	var requester_data = {
+		 'myArray': myArray
 	};
-  var source = $("#requester-template").html();
-  var template = Handlebars.compile(source);
-  var html = template(requester_data);
-  $("#content").html(html);
+
+	var source = $("#requester-template1").html();
+	var template = Handlebars.compile(source);
+	var html = template(requester_data);
+	$("#content").html(html);
 } 
+
+
+
+
+function sendMyData(myArray){
+	console.log("in my send data");
+	var requester_data = {
+		 'myArray': myArray
+	};
+	//console.log("requester_data = " + requester_data);
+	var source = $("#requester-template").html();
+	//console.log("source = " + source);
+	var template = Handlebars.compile(source);
+	//console.log("template = " + template);
+	var html = template(requester_data);
+	$("#content2").html(html);
+	//console.log("in my send data ends");
+}
+
